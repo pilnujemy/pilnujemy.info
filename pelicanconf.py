7158938b8d8e26e1345a072d1a3d4fc473f7a1a6
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-11-08 15:12:20 +0100

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Adam Dobrawy'
+SITENAME = u'Fundacja Pilnujemy.info'
+SITEURL = 'http://pilnujemy.info'
+THEME = 'theme'
+PATH = 'content'
+
+TIMEZONE = 'Europe/Warsaw'
+
+DEFAULT_LANG = u'pl'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# STATIC_PATHS = ['images', 'extra/CNAME']
+# EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
